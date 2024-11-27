8c1e72045656df56d1e565f79bd4701d6dca26e3
Intoxikate <nguyenkate47@gmail.com>
2024-11-27 03:44:08 -0500

    Created a Pieces parent class and classes for the remaining pieces.

--- pieces.py
+++ pieces.py
@@ -2,25 +2,136 @@
 import pygame as py
 import os
 from constants import SQUARE_SIZE, WHITE
+
+# Pawn
 white_pawn_image = py.image.load(os.path.join('./assets', 'white_pawn.png'))
 black_pawn_image = py.image.load(os.path.join('./assets', 'black_pawn.png'))
 
+# Rook
+white_rook_image = py.image.load(os.path.join('./assets', 'white_rook.png'))
+black_rook_image = py.image.load(os.path.join('./assets', 'black_rook.png'))
 
-class Pawn:
-    def __init__(self, canvas, row, col, color):
+# Knight
+white_knight_image = py.image.load(os.path.join('./assets', 'white_knight.png'))
+black_knight_image = py.image.load(os.path.join('./assets', 'black_knight.png'))
+
+# Bishop
+white_bishop_image = py.image.load(os.path.join('./assets', 'white_bishop.png'))
+black_bishop_image = py.image.load(os.path.join('./assets', 'black_bishop.png'))
+
+# Queen
+white_queen_image = py.image.load(os.path.join('./assets', 'white_queen.png'))
+black_queen_image = py.image.load(os.path.join('./assets', 'black_queen.png'))
+
+# King
+white_king_image = py.image.load(os.path.join('./assets', 'white_king.png'))
+black_king_image = py.image.load(os.path.join('./assets', 'black_king.png'))
+
+class Piece:
+    def __init__(self, canvas, row, col, color, image):
         self.row = row
         self.col = col
         self.color = color
         self.canvas = canvas
-        # Pygame starts drawing from the top left, so each piece needs its own
-        # offset to be in the center of the square.
+        self.image = image
         self.render_offset = 18
         self.pos = (col * SQUARE_SIZE + self.render_offset,
                     row * SQUARE_SIZE + self.render_offset * 2)
-        if (self.color == WHITE):
-            self.image = white_pawn_image
+
+class Pawn(Piece):
+    def __init__(self, canvas, row, col, color):
+        if (color == WHITE):
+            image = white_pawn_image
         else:
-            self.image = black_pawn_image
+            image = black_pawn_image
+        super().__init__(canvas, row, col, color, image)
+    
+    def move(self):
+        pass
+
+    def delete(self):
+        pass
+
+    # Pygame starts drawing from the top left, so each piece needs its own
+    # offset to be in the center of the square.
+    def render(self):
+        self.canvas.blit(self.image, self.pos)
+
+class Rook(Piece):
+    def __init__(self, canvas, row, col, color):
+        if (color == WHITE):
+            image = white_rook_image
+        else:
+            image = black_rook_image
+        super().__init__(canvas, row, col, color, image)
+
+    def move(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def render(self):
+        self.canvas.blit(self.image, self.pos)
+
+class Knight(Piece):
+    def __init__(self, canvas, row, col, color):
+        if (color == WHITE):
+            image = white_knight_image
+        else:
+            image = black_knight_image
+        super().__init__(canvas, row, col, color, image)
+
+    def move(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def render(self):
+        self.canvas.blit(self.image, self.pos)
+
+class Bishop(Piece):
+    def __init__(self, canvas, row, col, color):
+        if (color == WHITE):
+            image = white_bishop_image
+        else:
+            image = black_bishop_image
+        super().__init__(canvas, row, col, color, image)
+
+    def move(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def render(self):
+        self.canvas.blit(self.image, self.pos)
+
+class Queen(Piece):
+    def __init__(self, canvas, row, col, color):
+        if (color == WHITE):
+            image = white_queen_image
+        else:
+            image = black_queen_image
+        super().__init__(canvas, row, col, color, image)
+
+    def move(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def render(self):
+        self.canvas.blit(self.image, self.pos)
+
+class King(Piece):
+    def __init__(self, canvas, row, col, color):
+        if (color == WHITE):
+            image = white_king_image
+        else:
+            image = black_king_image
+        super().__init__(canvas, row, col, color, image)
 
     def move(self):
         pass
