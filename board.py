c8a9da76767d058d0ffb2c5b99fe2baf54450aa0
Eli Engelhart <eliengelhart@gmail.com>
2024-11-29 14:44:58 -0500

    Changed imports and render dead pieces.

--- board.py
+++ board.py
@@ -1,11 +1,11 @@
 # Contains the entire board class
 import pygame as py
-from pieces import Pawn
-from pieces import Rook
-from pieces import Knight
-from pieces import Bishop
-from pieces import Queen
-from pieces import King
+import pieces.pieces.Pawn
+import pieces.Rook
+import pieces.Knight
+import pieces.Bishop
+import pieces.Queen
+import pieces.King
 from constants import BOARD_HEIGHT, BOARD_WIDTH, ROWS, COLS, WHITE, BLACK, MINT, GREEN, SQUARE_SIZE
 
 
@@ -17,12 +17,24 @@ class Board:
         # self.board = self.init_board_and_pieces()
         self.board = self.init_board_and_pieces()
         self.render_pieces()
+        self.removed_white_pieces = [
+            [None for j in range(4)] for i in range(4)]
+        self.removed_black_pieces = [
+            [None for j in range(4)] for i in range(4)]
+        # for i in range(len(self.removed_black_pieces)):
+        #     for j in range(len(self.removed_black_pieces[0])):
+        #         self.removed_white_pieces[i][j] = pieces.Pawn(
+        #             self.canvas, 0, 0, WHITE)
+        #         self.removed_black_pieces[i][j] = pieces.Pawn( self.canvas, 0, 0, BLACK)
+        piece = pieces.pieces.Pawn(self.canvas, 0, 0, BLACK)
+        self.remove_piece_to_side_of_board(piece=piece)
 
     def render(self):
         self.canvas.fill(MINT)
         self.draw_squares()
         self.draw_board_outline()
         self.render_pieces()
+        self.render_removed_pieces()
 
     def draw_board_outline(self):
         py.draw.rect(self.canvas, BLACK,
@@ -45,51 +57,79 @@ class Board:
 
     def init_board_and_pieces(self):
         board = [[None for _ in range(8)] for _ in range(8)]
-        for i in range(8):
-            for j in range(8):
-                match i:
+        for row in range(8):
+            for col in range(8):
+                match row:
                     case 0:
                         # render black pieces
-                        new_piece = self.determine_piece_by_col(i, j)
+                        new_piece = self.init_piece_by_pos(row, col)
                     case 1:
-                        new_piece = Pawn(self.canvas, i, j, BLACK)
+                        new_piece = pieces.Pawn(self.canvas, row, col, BLACK)
                     case 6:
-                        new_piece = Pawn(self.canvas, i, j, WHITE)
+                        new_piece = pieces.Pawn(self.canvas, row, col, WHITE)
                     case 7:
-                        new_piece = self.determine_piece_by_col(i, j)
-                board[i][j] = new_piece
+                        new_piece = self.init_piece_by_pos(row, col)
+                board[row][col] = new_piece
         return board
 
     # Given a row and column, return the correct piece.
-    def determine_piece_by_col(self, i, j):
-        if i == 0:
+    def init_piece_by_pos(self, row, col):
+        if row == 0:
             color = BLACK
         else:
             color = WHITE
         # Select correct black piece
-        if j == 0 or j == 7:
-            return Rook(self.canvas, i, j, color)
-            pass
-        elif j == 1 or j == 6:
-            return Knight(self.canvas, i, j, color)
-            pass
-        elif j == 2 or j == 5:
-            return Bishop(self.canvas, i, j, color)
-            pass
-        elif j == 3:
-            return Queen(self.canvas, i, j, color)
-            pass
+        if col == 0 or col == 7:
+            return pieces.Rook(self.canvas, row, col, color)
+        elif col == 1 or col == 6:
+            return pieces.Knight(self.canvas, row, col, color)
+        elif col == 2 or col == 5:
+            return pieces.Bishop(self.canvas, row, col, color)
+        elif col == 3:
+            return pieces.Queen(self.canvas, row, col, color)
         else:
-            return King(self.canvas, i, j, color)
-            pass
+            return pieces.King(self.canvas, row, col, color)
 
     def render_pieces(self):
         for i in range(8):
             for j in range(8):
                 self.board[i][j].render()
 
+    def render_removed_pieces(self):
+        white_row_offset = 400
+        black_row_offset = 0
+        self.render_removed_pieces_helper(
+            self.removed_black_pieces, black_row_offset)
+        self.render_removed_pieces_helper(
+            self.removed_white_pieces, white_row_offset)
+
+    def render_removed_pieces_helper(self, removed_pieces, row_offset, col_offset=800):
+        for row in range(len(removed_pieces)):
+            for col in range(len(removed_pieces[0])):
+                # This case handles rendering a dead piece
+                piece = removed_pieces[row][col]
+                if piece is not None:
+                    self.canvas.blit(
+                        piece.image, ((col * 60 + col_offset),
+                                      (row * 100) + row_offset))
+
     def get_piece(self, row, col):
         if (row > 7 or col > 7):
             raise Exception(
                 "Uh-oh! Looks like the row or column was greater than 7. Please ensure that the row and column are less than 8.")
         return self.board[row][col]
+
+    def remove_piece_to_side_of_board(self, piece):
+        color = piece.color
+        if color == WHITE:
+            self.remove_piece_to_side_of_board(
+                self.removed_white_pieces, piece)
+        else:
+            self.remove_piece_to_side_of_board(
+                self.removed_black_pieces, piece)
+
+    def remove_piece_to_side_of_board_helper(self, removed_pieces, piece):
+        for row in range(len(removed_pieces)):
+            for col in range(len(removed_pieces[0])):
+                if removed_pieces[row][col] is not None:
+                    removed_pieces[row][col] = piece
